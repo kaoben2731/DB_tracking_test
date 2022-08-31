20c49545220f817717fc6ef2ada1d27781b4093a
KAOBEN_R9_5900X\kaobe <kaoben2731@gmail.com>
2022-09-01 00:14:28 +0800

    edit DB

--- python_dump.sql
+++ python_dump.sql
@@ -9,8 +9,9 @@ CREATE TABLE "test table 1" (
 );
 INSERT INTO "test table 1" VALUES(1,'Siemta','Toyota',7,1800);
 INSERT INTO "test table 1" VALUES(2,'Altis','Toyota',5,1800);
-INSERT INTO "test table 1" VALUES(3,'Odyssey','Homda',8,2400);
+INSERT INTO "test table 1" VALUES(3,'Odyssey','Homda',7,2400);
 INSERT INTO "test table 1" VALUES(4,'Delica','Mitsubishi',8,2400);
 INSERT INTO "test table 1" VALUES(5,'Staria','Hyundai',9,2200);
 INSERT INTO "test table 1" VALUES(6,'URX','納智捷',7,1800);
+INSERT INTO "test table 1" VALUES(7,'Berlingo','Citroen',7,1500);
 COMMIT;
